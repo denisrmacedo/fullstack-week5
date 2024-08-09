25142403e6ce0e732f422050ab98905bb75e7422
Denis Macedo <denisrmacedo@gmail.com>
2024-08-08 23:18:09 -0300

    feat: ensure barbershoop services are shown on barber page

--- src/app/barbershops/[id]/page.tsx
+++ src/app/barbershops/[id]/page.tsx
@@ -1,22 +1,29 @@
+import ServiceItem from "@/app/_components/service-item"
 import { Button } from "@/app/_components/ui/button"
 import { db } from "@/app/_lib/prisma"
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
-
+import { notFound } from "next/navigation"
 interface BarbershopPageProps {
   params: {
     id: string
   }
 }
-
 const BarbershopPage = async ({ params }: BarbershopPageProps) => {
-  // chamar o banco de dados
+  // chamar o meu banco de dados
   const barbershop = await db.barbershop.findUnique({
     where: {
       id: params.id,
     },
+    include: {
+      services: true,
+    },
   })
+
+  if (!barbershop) {
+    return notFound()
+  }
   return (
     <div>
       {/* IMAGEM */}
@@ -27,10 +34,9 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
           fill
           className="object-cover"
         />
-
         <Button
           size="icon"
-          variant={"secondary"}
+          variant="secondary"
           className="absolute left-4 top-4"
           asChild
         >
@@ -38,27 +44,21 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
             <ChevronLeftIcon />
           </Link>
         </Button>
-
         <Button
           size="icon"
-          variant={"secondary"}
+          variant="secondary"
           className="absolute right-4 top-4"
-          asChild
         >
-          <Link href="/">
-            <MenuIcon />
-          </Link>
+          <MenuIcon />
         </Button>
       </div>
-
       <div className="border-b border-solid p-5">
-        <h1 className="mb-3 text-xl font-bold">{barbershop?.name}</h1>
-        <div className="flex items-center gap-1">
+        <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
+        <div className="mb-2 flex items-center gap-2">
           <MapPinIcon className="text-primary" size={18} />
           <p className="text-sm">{barbershop?.address}</p>
         </div>
-
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-2">
           <StarIcon className="fill-primary text-primary" size={18} />
           <p className="text-sm">5,0 (499 avaliações)</p>
         </div>
@@ -68,8 +68,16 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
         <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
         <p className="text-justify text-sm">{barbershop?.description}</p>
       </div>
+
+      <div className="space-y-3 p-5">
+        <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
+        <div className="space-y-3">
+          {barbershop.services.map((service) => (
+            <ServiceItem key={service.id} service={service} />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
-
 export default BarbershopPage
